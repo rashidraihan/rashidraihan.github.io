e8bd107fa40bf8807748bab0f3c4fe89e4e6d8ce
Wraihan <rashidraihan021@gmail.com>
2026-08-31 00:47:33 +0600

    Fix random-post link, soften headings, add post subheadings and links

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // === CACHE BUSTING AND VERSION CONTROL ===
-const SITE_VERSION = '5.9';
+const SITE_VERSION = '6.1';
 
 if (localStorage.getItem('siteVersion') !== SITE_VERSION) {
   console.log('New version detected:', SITE_VERSION);
